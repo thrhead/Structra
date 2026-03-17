049c3d5b15bc17a8bb156323205d8459fc9831e2
tahir.kahraman85 <tahir.kahraman85@gmail.com>
2026-03-17 09:27:36 +0000

    fix: resolve 'React.Children.only' by preventing multiple children in Button asChild

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -37,10 +37,20 @@ export interface ButtonProps
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, isLoading, asChild = false, children, disabled, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
+    if (asChild) {
+      return (
+        <Slot
+          className={cn(buttonVariants({ variant, size, className }))}
+          ref={ref}
+          {...props}
+        >
+          {children}
+        </Slot>
+      )
+    }
 
     return (
-      <Comp
+      <button
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         disabled={disabled || isLoading}
@@ -53,7 +63,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
           </svg>
         )}
         {children}
-      </Comp>
+      </button>
     )
   }
 )
